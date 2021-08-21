81fb65a14363854c962dccd447542e81d389ca0a
François Nguyen <phurytw@outlook.com>
2021-08-21 02:30:13 +0200

    chore(3): prepare project for heroku, add login redirect on watching and pricing pages, add payment canceled and success pages (#13)

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-import { useQuery, gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useCallback, useEffect } from 'react'
+import useUser from '../hooks/useUser'
+import { UserState } from '../providers/UserProvider'
 import styles from './watch.module.scss'
 
 const GET_VIDEOS = gql`
@@ -21,6 +25,42 @@ export interface WorkoutVideo {
 
 export default function Watch() {
   const { data } = useQuery<{ workoutVideos: WorkoutVideo[] }>(GET_VIDEOS)
+
+  const { push } = useRouter()
+  const { me, isLoggedIn } = useUser()
+
+  const redirectAuthenticatedUser = useCallback(
+    function redirectAuthenticatedUser(user: UserState) {
+      const { subscription, subscriptionActive, subscriptionEnd, blocked } = user
+
+      if (
+        blocked ||
+        (subscription &&
+          subscriptionEnd &&
+          new Date(subscriptionEnd).getTime() > Date.now() &&
+          !subscriptionActive)
+      ) {
+        // is blocked or has his/her subscription terminated manually
+        push('/banned')
+      } else if (
+        !blocked &&
+        (!subscription ||
+          (subscription && subscriptionEnd && new Date(subscriptionEnd).getTime() < Date.now()))
+      ) {
+        // no active subscription or subscription expired
+        push('/pricing')
+      }
+    },
+    [push]
+  )
+
+  useEffect(() => {
+    if (isLoggedIn() && me) {
+      redirectAuthenticatedUser(me)
+    } else if (!isLoggedIn()) {
+      push('/pricing')
+    }
+  }, [me, isLoggedIn, push, redirectAuthenticatedUser])
 
   return (
     <div className={styles.Watch}>
--- pages/watch/[code].tsx
+++ pages/watch/[code].tsx
@@ -1,6 +1,9 @@
 import { useQuery, gql } from '@apollo/client'
 import { useRouter } from 'next/router'
+import { useCallback, useEffect } from 'react'
 import VdoCipherVideo from '../../components/VdoCipherVideo'
+import useUser from '../../hooks/useUser'
+import { UserState } from '../../providers/UserProvider'
 import styles from './[code].module.css'
 
 const GET_VIDEO_BY_CODE = gql`
@@ -24,7 +27,42 @@ export interface WatchVideoParams {
 }
 
 export default function WatchVideo() {
-  const { query } = useRouter()
+  const { query, push } = useRouter()
+
+  const { me, isLoggedIn } = useUser()
+
+  const redirectAuthenticatedUser = useCallback(
+    function redirectAuthenticatedUser(user: UserState) {
+      const { subscription, subscriptionActive, subscriptionEnd, blocked } = user
+
+      if (
+        blocked ||
+        (subscription &&
+          subscriptionEnd &&
+          new Date(subscriptionEnd).getTime() > Date.now() &&
+          !subscriptionActive)
+      ) {
+        // is blocked or has his/her subscription terminated manually
+        push('/banned')
+      } else if (
+        !blocked &&
+        (!subscription ||
+          (subscription && subscriptionEnd && new Date(subscriptionEnd).getTime() < Date.now()))
+      ) {
+        // no active subscription or subscription expired
+        push('/pricing')
+      }
+    },
+    [push]
+  )
+
+  useEffect(() => {
+    if (isLoggedIn() && me) {
+      redirectAuthenticatedUser(me)
+    } else if (!isLoggedIn()) {
+      push('/pricing')
+    }
+  }, [me, isLoggedIn, push, redirectAuthenticatedUser])
 
   const { data } = useQuery<{ workoutVideo: PremiumVideo }>(GET_VIDEO_BY_CODE, {
     variables: { id: query.code },
